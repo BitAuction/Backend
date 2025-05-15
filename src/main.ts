3125a80ecf7df9c8ac85344526a1c777d0187d16
AmrAhmed119 <es-AmrA.Abdelazim2025@alexu.edu.eg>
2025-05-15 16:31:47 +0300

    Enable CORS for specified local origins

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,19 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const allowedOrigins = [
+    'http://localhost:3000', 
+    'http://localhost:3001', 
+    'http://localhost:3002',
+    'http://localhost:3003',
+  ];
+
+  app.enableCors({
+    origin: allowedOrigins,
+    credentials: true,
+  });
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
